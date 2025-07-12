1b3a1accd32d2deefe1902974dea17b859537e02
Sushil-Jadhav07 <sushiljadhav407@gmail.com>
2025-07-12 11:13:14 +0530

    productpg banner

--- src/components/Shop/Details/ProductBanner.js
+++ src/components/Shop/Details/ProductBanner.js
@@ -11,12 +11,12 @@ const SnapOnBanner = ({productDetails}) => {
       {/* Original Section */}
       <div className="max-w-full flex flex-col lg:flex-row items-center">
         {/* Text Content */}
-        <div className="lg:pl-8 text-center lg:text-left lg:w-1/2 ">
+        <div className="lg:pl-12 text-center lg:text-left lg:w-1/2 ">
           <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-black mb-6">
-            SNAPON ENVELOPE SLEEVE
+            {productDetails.bannerTitleOne}
           </h2>
           <p className="text-base md:text-lg text-gray-700 max-w-xl mb-8 lg:mb-0">
-            Carefully crafted with premium quality, pebbled leatherite, the SnapOn Envelope Sleeve offers all-round protection to your laptop.
+            {productDetails.bannerDescOne}
           </p>
         </div>
 
@@ -48,12 +48,12 @@ const SnapOnBanner = ({productDetails}) => {
         </div>
 
         {/* Text Content */}
-        <div className="lg:pl-8 text-center lg:text-left lg:w-1/2">
+        <div className="lg:pl-12 text-center lg:text-left lg:w-1/2">
           <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-black mb-6">
-            STAINLESS STEEL BOTTLE
+           {productDetails.bannerTitleTwo}
           </h2>
           <p className="text-base md:text-lg text-gray-700 max-w-xl mb-8 lg:mb-0">
-            Durable and stylish, this stainless steel bottle keeps your beverages hot or cold for hours, perfect for everyday hydration on the go.
+            {productDetails.bannerDescTwo}          
           </p>
         </div>
       </div>
